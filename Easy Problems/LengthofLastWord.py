61b202bd332f8473ab4baf8a54eddfdc2c364712
Darpan Mehra <darpan.mehra10@gmail.com>
2019-05-28 11:32:16 +0530

    Solution: Length of Last Word Problem

---
+++ Easy Problems/LengthofLastWord.py
@@ -0,0 +1,19 @@
+#Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+#If the last word does not exist, return 0.
+
+#Note: A word is defined as a character sequence consists of non-space characters only.
+
+#Example:
+
+#Input: "Hello World"
+#Output: 5
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        if s.strip() == '':
+            return 0
+        lastword=s.strip().split(' ')[-1]
+        return len(lastword) if lastword!= '' else 1
+
+#Runtime: 36 ms, faster than 86.84% of Python3 online submissions for Length of Last Word.
+#Memory Usage: 13.1 MB, less than 68.81% of Python3 online submissions for Length of Last Word.
